7ac254a92d12f5fbff997bf619981767a2a84f0e
René Herrero <rene@sevenseas.io>
2024-05-11 05:15:02 -0500

    added the udp socket protect

--- cyphal-udp/src/lib.rs
+++ cyphal-udp/src/lib.rs
@@ -1,1 +1,5 @@
+//! Open Cyphal UDP Rust Implementation
+//!
 #![no_std]
+#![allow(async_fn_in_trait)]
+#![warn(missing_docs)]
---
+++ cyphal-udpsocket/src/lib.rs
@@ -0,0 +1,3 @@
+//! Open Cyphal UDP Socket Rust Implementation
+#![allow(async_fn_in_trait)]
+#![warn(missing_docs)]
