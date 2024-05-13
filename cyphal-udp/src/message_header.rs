adf6236192af0dc95ba7d62bef434186e8ec06bb
René Herrero <rene@sevenseas.io>
2024-05-12 20:07:24 -0500

    partially implemented headers

--- cyphal-udp/src/message_header.rs
+++ cyphal-udp/src/message_header.rs
@@ -1,29 +1,126 @@
-use crate::{UdpError, UdpNodeId, UdpResult};
-use cyphal::Priority;
+use crate::{UdpError, UdpNodeId, UdpResult, UdpSubjectId, UdpTransferId};
+use cyphal::{CyphalError, CyphalResult, NodeId, Priority, SubjectId, TransferId};
 
 /// Represents a UDP payload header used for messages
-#[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Copy, Clone)]
 pub struct MessageHeader {
-    version: u8,
     priority: Priority,
-    source: UdpNodeId,
-    destination: u16,
-    data_specifier_snm: u16,
-    transfer: u64,
+    source: Option<UdpNodeId>,
+    destination: Option<UdpNodeId>,
+    subject: UdpSubjectId,
+    transfer: UdpTransferId,
     index: u32,
-    user_data: u16,
+    end_of_transfer: bool,
     crc16: [u8; 2],
 }
 
 impl MessageHeader {
+    /// Constructs a new message header
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        priority: Priority,
+        source: Option<UdpNodeId>,
+        destination: Option<UdpNodeId>,
+        subject: UdpSubjectId,
+        transfer: UdpTransferId,
+        index: u32,
+        end_of_transfer: bool,
+        crc16: [u8; 2],
+    ) -> CyphalResult<Self> {
+        if index > 0x7FFFFFFF {
+            return Err(CyphalError::OutOfRange);
+        }
+
+        Ok(Self {
+            priority,
+            source,
+            destination,
+            subject,
+            transfer,
+            index,
+            end_of_transfer,
+            crc16,
+        })
+    }
+
     /// Returns the priority of the message
     pub fn priority(&self) -> Priority {
         self.priority
     }
 
+    /// Returns the source of the message  'None' is returned if it's anonymous
+    pub fn source(&self) -> Option<UdpNodeId> {
+        self.source
+    }
+
+    /// Returns the destination of the message.  'None' is returned if it's a broadcast
+    pub fn destination(&self) -> Option<UdpNodeId> {
+        self.destination
+    }
+
+    /// Returns the subject of the message
+    pub fn subject(&self) -> UdpSubjectId {
+        self.subject
+    }
+
+    /// Returns the transfer ID of the message
+    pub fn transfer(&self) -> UdpTransferId {
+        self.transfer
+    }
+
+    /// Returns the index of the message
+    pub fn index(&self) -> u32 {
+        self.index
+    }
+
+    /// Returns `true` if it's the end of the message
+    pub fn end_of_transfer(&self) -> bool {
+        self.end_of_transfer
+    }
+
+    /// Returns the crc16 of the message
+    pub fn crc16(&self) -> &[u8; 2] {
+        &self.crc16
+    }
+
     /// Returns a `&[u8; 24]` representation of the message header
     pub fn as_raw(&self) -> [u8; 24] {
-        todo!()
+        let source: [u8; 2] = match self.source {
+            Some(i) => [(i.value() >> 4) as u8, (i.value() & 0xFF) as u8],
+            None => [0xFF, 0xFF],
+        };
+        let destination: [u8; 2] = match self.destination {
+            Some(i) => [(i.value() >> 4) as u8, (i.value() & 0xFF) as u8],
+            None => [0xFF, 0xFF],
+        };
+        let transfer = self.transfer.value();
+        let index_mask: u8 = if self.end_of_transfer { 1 } else { 0 };
+        [
+            1,
+            self.priority as u8,
+            source[0],
+            source[1],
+            destination[0],
+            destination[1],
+            (self.subject.value() >> 3) as u8,
+            ((self.subject.value() & 0x7F) << 1) as u8,
+            (transfer >> 28) as u8,
+            ((transfer >> 24) & 0xFF) as u8,
+            ((transfer >> 20) & 0xFF) as u8,
+            ((transfer >> 16) & 0xFF) as u8,
+            ((transfer >> 12) & 0xFF) as u8,
+            ((transfer >> 8) & 0xFF) as u8,
+            ((transfer >> 4) & 0xFF) as u8,
+            (transfer & 0xFF) as u8,
+            (self.index >> 3) as u8,
+            ((self.index & 0x7F) << 1) as u8 | index_mask,
+            0,
+            0,
+            self.crc16[0],
+            self.crc16[1],
+            0,
+            0,
+        ]
     }
 }
 
--- cyphal-udp/src/service_header.rs
+++ cyphal-udp/src/service_header.rs
@@ -1,24 +1,94 @@
-use crate::{UdpError, UdpResult};
-use cyphal::Priority;
+use crate::{UdpError, UdpNodeId, UdpResult, UdpServiceId, UdpTransferId};
+use cyphal::{CyphalError, CyphalResult, Priority};
 
 /// Represents a payload header used for services
-#[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Copy, Clone)]
 pub struct ServiceHeader {
-    version: u8,
     priority: Priority,
-    source: u16,
-    destination: u16,
-    data_specifier_snm: u16,
-    transfer: u64,
+    source: UdpNodeId,
+    destination: UdpNodeId,
+    is_request: bool,
+    service_id: UdpServiceId,
+    transfer: UdpTransferId,
     index: u32,
-    user_data: u16,
+    end_of_transfer: bool,
     crc16: [u8; 2],
 }
 
 impl ServiceHeader {
+    /// Constructs a new service header
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        priority: Priority,
+        source: UdpNodeId,
+        destination: UdpNodeId,
+        is_request: bool,
+        service_id: UdpServiceId,
+        transfer: UdpTransferId,
+        index: u32,
+        end_of_transfer: bool,
+        crc16: [u8; 2],
+    ) -> CyphalResult<Self> {
+        if index > 0x7FFFFFFF {
+            return Err(CyphalError::OutOfRange);
+        }
+
+        Ok(Self {
+            priority,
+            source,
+            destination,
+            is_request,
+            service_id,
+            transfer,
+            index,
+            end_of_transfer,
+            crc16,
+        })
+    }
+
     /// Returns the priority of the service
     pub fn priority(&self) -> Priority {
         self.priority
+    }
+
+    /// Returns the source of the service
+    pub fn source(&self) -> UdpNodeId {
+        self.source
+    }
+
+    /// Returns the destination of the service
+    pub fn destination(&self) -> UdpNodeId {
+        self.destination
+    }
+
+    /// Returns the service ID
+    pub fn service_id(&self) -> UdpServiceId {
+        self.service_id
+    }
+
+    /// Returns `true` if it's a request of the service
+    pub fn is_request(&self) -> bool {
+        self.is_request
+    }
+
+    /// Returns the transfer ID of the service
+    pub fn transfer(&self) -> UdpTransferId {
+        self.transfer
+    }
+
+    /// Returns the index of the service
+    pub fn index(&self) -> u32 {
+        self.index
+    }
+
+    /// Returns `true` if it's the end of the service
+    pub fn end_of_transfer(&self) -> bool {
+        self.end_of_transfer
+    }
+
+    /// Returns the crc16 of the service
+    pub fn crc16(&self) -> &[u8; 2] {
+        &self.crc16
     }
 
     /// Returns a `u32` representation of the service header
