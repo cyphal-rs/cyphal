b6ba98e6d559106f24503d9f2b9e994eeca01c41
René Herrero <rene@sevenseas.io>
2024-05-18 15:58:22 -0500

    updated message produced by cli

--- dsdl/src/generate/message.rs
+++ dsdl/src/generate/message.rs
@@ -1,11 +1,54 @@
 use clap::error::Result as ClapResult;
 
-const MESSAGE: &str = r#"pub struct {NAME} {
+const MESSAGE: &str = r#"
+use cyphal::{CyphalResult, Message, NodeId, Priority, SubjectId};
+
+const MESSAGE_SIZE: usize = {SIZE};
+
+/// Represents a {NAME} message
+pub struct {NAME} {
     priority: Priority,
     subject: SubjectId,
     source: Option<NodeId>,
-    payload: [u8; {SIZE}],
-}"#;
+    payload: [u8; MESSAGE_SIZE],
+}
+
+impl {NAME} {
+    pub fn raw(
+        priority: Priority,
+        subject: SubjectId,
+        source: Option<NodeId>,
+        data: [u8; MESSAGE_SIZE],
+    ) -> CyphalResult<Self> {
+        Ok(Self {
+            priority,
+            subject,
+            source,
+            data,
+        })
+    }
+}
+
+impl Message for TestSmallMessage {
+    const SIZE: usize = MESSAGE_SIZE;
+
+    fn priority(&self) -> Priority {
+        self.priority
+    }
+
+    fn subject(&self) -> SubjectId {
+        self.subject
+    }
+
+    fn source(&self) -> Option<NodeId> {
+        self.source
+    }
+
+    fn data(&self) -> &[u8] {
+        &self.data
+    }
+}
+"#;
 
 pub struct MessageGenerator {
     name: Option<String>,
